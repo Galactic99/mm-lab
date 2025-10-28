a31b18b089d715c02dc27091d8b0359d9ec8d10d
vaishnav <vaishnavkartha2005@gmail.com>
2025-10-28 09:14:03 +0530

    s

---
+++ relocating_loader.c
@@ -0,0 +1,152 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+char bitmask[12],bit[12]={0};
+
+void convert(char h[12]){
+    int i,l;
+    strcpy(bit,"");
+    l=strlen(h);
+
+
+    for(i=0;i<l;i++){
+        switch(h[i]){
+            case '0':
+                strcat(bit,"0000");
+                break;
+
+            case '1':
+                strcat(bit,"0001");
+                break;
+
+            case '2':
+                strcat(bit,"0010");
+                break;
+
+
+            case '3':
+                strcat(bit,"0011");
+                break;
+
+            case '4':
+                strcat(bit,"0100");
+                break;
+
+            case '5':
+                strcat(bit,"101");
+                break;
+
+
+            case '6':
+                strcat(bit,"110");
+                break;
+
+            case '7':
+                strcat(bit,"111");
+                break;
+
+            case '8':
+                strcat(bit,"1000");
+                break;
+
+            case '9':
+                strcat(bit,"1001");
+                break;
+
+
+            case 'A':
+                strcat(bit,"1010");
+                break;
+
+
+            case 'B':
+                strcat(bit,"1011");
+                break;
+
+            case 'C':
+                strcat(bit,"1100");  
+                break;
+
+            case 'D':
+                strcat(bit,"1101");
+                break;
+
+            case 'E':
+                strcat(bit,"1110");
+                break;
+
+            case 'F':
+                strcat(bit,"1111");
+                break;
+        }
+    }
+}
+
+
+int main(){
+    char add[6],length[10],input[10],binary[12],relocbit,ch,pn[10];
+    int start,inp,len,i,address,opcode,addr,actualadd,tlen;
+
+    FILE *fp1,*fp2;
+
+    fp1=fopen("rel_input.txt","r");
+    fp2=fopen("rel_output.txt","w");
+
+    printf("Enter the actual address: ");
+    scanf("%x",&start);
+
+
+    //fscanf(fp1,"%s",input);
+
+
+    while(fscanf(fp1,"%9s",input)==1){
+        if (strcmp(input, "E") == 0) break;
+        if(strcmp(input,"H")==0){
+            fscanf(fp1,"%s",pn);
+            fscanf(fp1,"%x",&addr);
+            fscanf(fp1,"%x",&len);
+            //fscanf(fp1,"%s",input);
+        }
+
+        else if(strcmp(input,"T")==0){
+            fscanf(fp1,"%x",&address);
+            fscanf(fp1,"%x",&tlen);
+            fscanf(fp1,"%s",bitmask);
+
+
+            address+=start;
+
+            convert(bitmask);
+
+
+            len=strlen(bit);
+
+            if(len >=12){
+                len=10;
+            }
+
+            for(i=0;i<len;i++){
+                fscanf(fp1,"%x",&opcode);
+                fscanf(fp1,"%x",&addr);
+                relocbit=bit[i];
+
+                if(relocbit=='0'){
+                    actualadd=addr;
+                }else{
+                    actualadd=start+addr;
+                }
+                fprintf(fp2,"%x\t\t%x%x\n",address,opcode,actualadd);
+                address+=3;
+            }
+
+            //fscanf(fp1,"%s",input);
+
+        }
+
+    }
+    fclose(fp1);
+    fclose(fp2);
+
+    printf("finished");
+}
